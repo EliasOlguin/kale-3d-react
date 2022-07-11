a7a6e7f4d2d397a4aa5b6bf0ee68f3572ef0d775
EliasOlg <eliasjolguin@gmail.com>
2022-07-11 09:36:21 -0300

    Boton count e ir al carrito

--- src/Components/Containers/Items/Item.js
+++ src/Components/Containers/Items/Item.js
@@ -1,20 +1,28 @@
 import { CardActionArea, CardActions, CardContent, Link, Typography } from '@mui/material'
 import { Button } from 'bootstrap'
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Card, CardImg } from 'react-bootstrap'
 import { NavLink } from 'react-router-dom'
+import { ItemCount } from '../ItemCount/ItemCount'
 import "./Item.css"
 
 export const Item = ({producto,i}) => {
+  const [stock, setStock] = useState(producto.stock)
+  const [comprar, setComprar] = useState(false)
+  const onAdd = (count) =>{
+    setComprar(true)
+    setStock(count)
+  }
+  // useEffect(() => {
+  //   if(producto.stock == 10 & stock == 10){
+  //     setComprar(false)
+  //     setStock(stock)
+      
+  //   }  
+  // }, [producto])
+  
   
   return (
-    // <div>
-    //       <img src={producto.img} alt={producto.img} />
-    //       <h2>{producto.nombre} </h2>
-    //       <h4>{producto.precio} </h4>
-    //       <h4>{producto.stock} </h4>      
-    //       <Link to={"/product/"+producto.id} > Ver más </Link>
-    // </div>
     <Card style={{width:"18rem"}}>
       <CardActionArea>
         <CardImg src={producto.img} variant="top" alt={producto.nombre}  />
@@ -30,16 +38,26 @@ export const Item = ({producto,i}) => {
             Precio:{producto.precio}
           </Typography>
           <Typography variant='body2' color="text.secondary">
-            Stock:{producto.stock}
+            Stock:{stock}
           </Typography>
         </CardContent>
       </CardActionArea>
-      <CardActions>
+      <CardActions className='col'>
         <NavLink to={"/product/"+producto.id}>
-          <button className='btn btn-primary'>
+          <button className='btn btn-warning'>
             Ver detalles
           </button>
         </NavLink>
+        <div>
+          {
+            comprar?  <NavLink to={"/cart"}>
+              <button className='btn btn-success'>
+                Ir al carrito
+              </button>
+              </NavLink>:<ItemCount stock={producto.stock} onAdd={onAdd}/>
+          }
+        </div>
+        
       </CardActions>
 
     </Card>
